e61a786ef2cdf26a6b1a2004f380ec9fcbf77646
marvellousabi <marvellousabiola08@gmail.com>
2026-08-16 16:47:27 +0100

    new

--- vite.config.ts
+++ vite.config.ts
@@ -14,15 +14,15 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/auth': {
-        target: 'https://fraud-api.ordfellow.com',
+        target: 'https://fraudguard-mddi.onrender.com',
         changeOrigin: true,
       },
       '/api': {
-        target: 'https://fraud-api.ordfellow.com',
+        target: 'https://fraudguard-mddi.onrender.com',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'https://fraud-api.ordfellow.com',
+        target: 'https://fraudguard-mddi.onrender.com',
         ws: true,
       },
     },
